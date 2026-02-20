dae71d04f8a51646e4066ddbedac479b3f8c79b0
kanuprajapati <kanuprajapati717@gmail.com>
2026-02-20 15:24:29 +0000

    chenge

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 
 // CORS configuration
 app.use(cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    origin: process.env.CLIENT_URL || 'https://hridved-opal.vercel.app/',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
