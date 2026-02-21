2f8b510836198f19c4d43d2fea2222960c7538fe
satvamarketing <satvamarketing1@gmail.com>
2026-02-21 13:34:02 +0530

    clgfd

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -3,6 +3,12 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+console.log('Initializing Cloudinary with:', {
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME ? 'PRESENT' : 'MISSING',
+    api_key: process.env.CLOUDINARY_API_KEY ? 'PRESENT' : 'MISSING',
+    api_secret: process.env.CLOUDINARY_API_SECRET ? 'PRESENT' : 'MISSING'
+});
+
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
